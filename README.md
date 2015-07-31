4b0ebcc4732b4b10b3dbdc1b26cefa645a3c8d30
Cristian Pallarés <skyrpex@gmail.com>
2015-07-31 16:01:46 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,32 @@
+# Gulp Less Watcher
+
+Wraps Webpack into a streaming interface (and generates sourcemaps).
+
+## Usage
+
+```bash
+npm install gulp-webpack-sourcemaps --save-dev
+```
+
+```javascript
+import gulp        from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+
+const $ = loadPlugins();
+const config = {
+  // Enable watch mode
+  watch: true,
+  // Other Webpack config...
+};
+
+// We can safely use read: false
+return gulp.src('main.less', { cwd: 'resources/assets/styles/', read: false, })
+  .pipe($.sourcemaps.init()) // Enable sourcemaps
+  .pipe($.webpackSourcemaps(config))
+  .pipe($.sourcemaps.write('./', { includeContent: true }))
+  .pipe(gulp.dest('public/assets/'));
+```
+
+## Future changes
+
+Maybe it's worth
---
+++ src/index.js
@@ -0,0 +1,96 @@
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+import through2 from 'through2';
+import tempfile from 'tempfile';
+import deepmerge from 'deepmerge';
+import applySourceMap from 'vinyl-sourcemaps-apply';
+
+const compile = (options = {}) => {
+
+  return through2.obj(function(file, enc, callback) {
+
+    const filename = tempfile('.js');
+    const sourcemap = `${filename}.map`;
+
+    let config = deepmerge(options, {
+      entry: file.path,
+      output: {
+        path: path.dirname(filename),
+        filename: path.basename(filename),
+      },
+    });
+
+    const useSourcemaps = file.sourceMap == true;
+
+    if (useSourcemaps) {
+      config = deepmerge(config, {
+        devtool: 'sourcemap',
+        output: {
+          sourcemapFilename: path.basename(sourcemap),
+          devtoolModuleFilenameTemplate(info) {
+            const filename = path.relative(process.cwd(), info.absoluteResourcePath);
+            return filename;
+          },
+        },
+      });
+    }
+
+    const compiler = webpack(config);
+
+    const done = (err, stats) => {
+
+      if (err) {
+        this.emit('error', err);
+        return;
+      }
+
+      if (stats.hasErrors()) {
+        stats.compilation.errors.forEach(error => {
+          this.emit('error', error);
+        });
+
+        if (!options.watch) {
+          this.emit('end');
+        }
+
+        return;
+      }
+
+      if (stats.hasWarnings()) {
+        stats.compilation.warnings.forEach(warning => {
+          this.emit('warning', error);
+        });
+      }
+
+      const clone = file.clone();
+
+      const contents = fs.readFileSync(filename);
+
+      clone.contents = contents;
+      if (clone.sourceMap && useSourcemaps) {
+        const map = fs.readFileSync(sourcemap).toString();
+        applySourceMap(clone, map);
+      }
+
+      this.push(clone);
+
+      if (!options.watch) {
+        callback();
+      }
+
+    };
+
+    if (options.watch) {
+      compiler.watch({}, done);
+    } else {
+      compiler.run(done);
+    }
+
+  });
+
+};
+
+compile.webpack = webpack;
+
+export default compile;
